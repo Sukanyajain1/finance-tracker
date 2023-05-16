87f8eabe38d3b877c37ea5f72d5c3470ca413afd
Sukanya <sukanyajain1@gmail.com>
2023-05-16 17:37:16 -0400

    register cookie parser and json webtoken package installed

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 require("dotenv").config(); //so that we can read the information about our secret key which is stored in the .env file
+const cookieParser = require("cookie-parser"); //so tha tthe server can understand the cookie information coming in from the client (browser)
 
 console.log("SECRET KEY IS THIS---> ", process.env.SECRET_KEY)
 const app = express();
@@ -9,7 +10,8 @@ const port = 8000;
 
 app.use(express.json());
 app.use(express.urlencoded({extended :true}));
-app.use(cors());
+app.use(cors({credentials: true, origin: "http://localhost:3000"}));
+app.use(cookieParser());
 
 
 require("./server/config/mongoose.config");
