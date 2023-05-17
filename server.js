9209996a6a8acf3a36f9fb19fe3d80212f4ae51f
Sukanya <sukanyajain1@gmail.com>
2023-05-16 21:08:54 -0400

    completed login/reg front end

--- server.js
+++ server.js
@@ -1,26 +1,24 @@
 const express = require("express");
 const cors = require("cors");
 
-require("dotenv").config(); //so that we can read the information about our secret key which is stored in the .env file
-const cookieParser = require("cookie-parser"); //so tha tthe server can understand the cookie information coming in from the client (browser)
+require('dotenv').config(); //so that we can read the information about our secret key which is stored in the .env file
+const cookieParser = require('cookie-parser'); //so that the server can understand the cookie information coming in from the client (browser)
 
-console.log("SECRET KEY IS THIS---> ", process.env.SECRET_KEY)
-const app = express();
+const app = express(); 
 const port = 8000;
 
 app.use(express.json());
-app.use(express.urlencoded({extended :true}));
-app.use(cors({credentials: true, origin: "http://localhost:3000"}));
+app.use(express.urlencoded({extended:true})) 
+app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
 app.use(cookieParser());
+
 
 
 require("./server/config/mongoose.config");
 
 
 
-
-require("./server/routes/user.routes")(app);
-
+require('./server/routes/user.routes')(app);
 
 
 app.listen(port, ()=> console.log(`<3 Hi, there! Listening on port: ${port}`));
--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,28 +1,64 @@
-const mongoose = require("mongoose");
-const bcrypt = require('bcrypt');
+const mongoose = require ('mongoose');
+const bcrypt = require("bcrypt");
+
 
 const UserSchema = new mongoose.Schema ({
     firstName: {
         type: String,
-        required: [true, "First name is required."]
+        required: [true, 'First name is required']
     },
     lastName: {
         type: String,
-        required: [true, "Last name is required."]
+        required: [true, 'Last name is required']
     },
     email: {
         type: String,
-        required: [true, "Email is required."],
+        required: [true, 'Email is required'],
         validate: {
-            validator: val => /^([\w\.]+@([\w-]+\.)+[\w-]+)?$/.test(val), //tests the value from form with this regular expression pattern to validate if the pattern is in an email format.
-            message: "Please enter a valid email."
+            validator: val => /^([\w-\.]+@([\w-]+\.)+[\w-]+)?$/.test (val), //tests the value from form with this regular expression pattern to validate if the pattern is in an email format
+            message: 'Please enter a valid email',
         }
     },
     password: {
         type: String,
-        required: [true, "Password is required."],
-        minlength: [8, "Password must be 8 characters or longer."]
+        required: [true, 'Password is required'],
+        minlength: [8, 'Password must be 8 characters or longer'],
+    },
+},{timestamps: true});
+
+
+//create a virtual field called "confirm" that is used just to validate the password matches confirm--> the getter and setter above are just creating temporary fields for _confirm
+UserSchema.virtual('confirm')
+    .get( () => this._confirm )
+    .set( value => this._confirm = value ); 
+
+
+
+//before (pre) running the other validations on the model the user to the db, validate the user objects password matches. If they dont match, this.invalidate() will create a validation error message 
+UserSchema.pre('validate', function(next) {
+    if (this.password !== this.confirm) {
+        this.invalidate('confirm', 'Password must match confirm password');
     }
+    next(); //after the above process is done, go to the next usual step
+    });
+
+
+//before (pre) saving the user to the db (this means we have passed the validations), hash the users password (encrypt it)
+UserSchema.pre('save', function(next) {
+    bcrypt.hash(this.password, 10)
+        .then(hash => {
+        this.password = hash;
+        next();
+        });
+});
+
+
+
+
+module.exports = mongoose.model ('User', UserSchema);
+
+
+
     // address: {
     //     street: {
 
@@ -39,32 +75,3 @@ const UserSchema = new mongoose.Schema ({
     //     type: Number,
     //     // error message
     // }
-}, {timestamps: true});
-
-
-// create a virtual field called "confirmPassword" that is used just to validate the password matches confirm--> the getter and setter above are just creating temporary fields for _confirm.
-UserSchema.virtual('confirmPassword')
-    .get( () => this._confirmPassword ) //
-    .set( value => this._confirmPassword = value );
-
-
-
-// before (pre) saving the user to the db, validate the user objects password matches. if they dont match, this.invalidate() will create a validation error message
-UserSchema.pre('validate', function(next) {
-    if (this.password !== this.confirmPassword) {
-        this.invalidate('confirmPassword', 'Password must match confirm password');
-    }
-    next(); }); //after the above process is done, go to the next usual step
-
-
-
-// before (pre) saving the user to the db (this means we have passed the validations), has the users password (encrypt it)
-UserSchema.pre('save', function(next) {
-    bcrypt.hash(this.password, 10) //10 salt rounds
-        .then(hash => {
-            this.password = hash;
-            next(); });
-    });
-
-
-module.exports = mongoose.model("User", UserSchema);
